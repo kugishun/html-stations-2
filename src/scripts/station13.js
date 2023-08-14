eaa537ac107b5c407e219b5b1d508bcc31d91f77
kugimiya shun <s1290195@u-aizu.ac.jp>
2023-08-14 16:28:52 +0900

    clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,23 @@
+// const { values } = import("cypress/types/lodash")
+
+// function changeBackgroundColor(e) {
+//   // ここに背景色を変える処理を書く。
+//   const check = document.getElementById("check")
+//   const text = document.getElementById("text")
+//   console.log(check.checked)
+//   if(check.checked){
+//     text.style.backgroundColor ="red"
+//   }
+//   else text.style.backgroundColor = ""
+// }
+
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const text = document.getElementById("text");
+  const button = document.getElementById("check");
+  if (button.checked) {
+    text.style.backgroundColor = "red";
+  } else {
+    text.style.backgroundColor = "white";
+  }
 }
